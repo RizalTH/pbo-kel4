1eb1f291eca33dd3d0573d2d791cf3a8fb237c90
RizalTH <57531111+RizalTH@users.noreply.github.com>
2019-11-15 10:27:33 +0700

    Add files via upload

---
+++ src/GameMath/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameMath;
+
+/**
+ *
+ * @author Rizal
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        m.mulaiGame(m);
+        m.menu(m);
+    }
+}
